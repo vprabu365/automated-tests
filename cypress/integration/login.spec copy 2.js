2cf34163d8849ef0cf703795ed168f8384ec6744
vprabu365 <vprabu@goguardian.com>
2021-07-27 10:22:44 -0400

    config 33

--- cypress/integration/login.spec copy 2.js
+++ cypress/integration/login.spec copy 2.js
@@ -1,10 +1,10 @@
 describe(`login scenario using task`, () => {
     before(() => {
-       // cy.visit(`/home`);
-        cy.task(`getSession`, {username: `studentm@dev-edu-peardeck.com`, password: `howdy235`, url: `https://qa.peardeck.dev/authPicker?finalDestinationUrl=%2Fhome%2F`}).then(session => {
-            cy.restoreSession(session);
-        })
-       // cy.get('[data-qa="homeLinkTop"]').should('be.visible')
+    //    // cy.visit(`/home`);
+    //     cy.task(`getSession`, {username: `studentm@dev-edu-peardeck.com`, password: `howdy235`, url: `https://qa.peardeck.dev/authPicker?finalDestinationUrl=%2Fhome%2F`}).then(session => {
+    //         cy.restoreSession(session);
+    //     })
+    //    // cy.get('[data-qa="homeLinkTop"]').should('be.visible')
     })
     it(`login test`, () => {
         cy.visit(`/home`);
