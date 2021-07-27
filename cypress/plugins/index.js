34df01e3ad18e3945023e7da67b474d31f22ca1c
vprabu365 <vprabu@goguardian.com>
2021-07-26 21:47:15 -0400

    change config5

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,3 +1,5 @@
+import { GetSession } from '../../session';
+
 /// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
@@ -15,7 +17,8 @@
 /**
  * @type {Cypress.PluginConfig}
  */
-import { GetSession } from '../../session';
+
+
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
