bb8ac56b6267a221f96c47396eec5600182a0b86
vprabu365 <vprabu@goguardian.com>
2021-07-27 17:16:39 -0400

    latest100

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,6 +23,11 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.add(`login`,function () {
+	cy.task(`getSession`, {username: `studentm@dev-edu-peardeck.com`, password: `howdy235`, url: `https://qa.peardeck.dev/authPicker?finalDestinationUrl=%2Fhome%2F`}).then(session => {
+		cy.restoreSession(session);
+	})
+});
 
 Cypress.Commands.add(`restoreSession`, function ({cookies, lsd, ssd}) {
 	const wList = [];
