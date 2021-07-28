b2292d6ba00e376b00a5b15a2a3a9ea96bc209d4
vprabu365 <vprabu@goguardian.com>
2021-07-27 20:55:35 -0400

    dasd

--- cypress/plugins/session.js
+++ cypress/plugins/session.js
@@ -105,7 +105,7 @@ module.exports = {
       //     waitUntil: `networkidle`,
       //     waitUntil: 'load' 
       // });
-        //await page.close()
+       await browser.close();
         return {
             cookies,
             lsd,
