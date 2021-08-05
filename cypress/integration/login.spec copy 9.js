99ba4b32fd130e714d324c94c74e28d161933385
vprabu365 <vprabu@goguardian.com>
2021-08-05 15:47:12 -0400

    run multiple login/logout tests

---
+++ cypress/integration/login.spec copy 9.js
@@ -0,0 +1,14 @@
+describe(`login scenario using task`, () => {
+
+    it(`login test`, () => {
+        cy.login();
+        cy.visit(`/home`);
+        cy.get('[data-qa="pdActivitiesHeader"]').should('be.visible')
+        cy.get("[class='c-nav__profile__menu-arrow']").click();
+        cy.get("[href='/auth/logout']").scrollIntoView().should('be.visible')
+        cy.get("[href='/auth/logout']").click()
+        cy.get('[data-qa="googleButton"]').should('be.visible')
+
+        
+    })
+});
